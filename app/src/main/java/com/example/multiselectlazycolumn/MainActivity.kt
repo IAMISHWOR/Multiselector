a5a963c96ac9e0a1f438127797b27c46c83f45c4
ishwor khadka <ishworback123@gmail.com>
2023-02-04 23:41:15 +0545

    Finished project

---
+++ app/src/main/java/com/example/multiselectlazycolumn/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.multiselectlazycolumn
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.ModifierLocalReadScope
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.multiselectlazycolumn.ui.theme.MultiSelectLazyColumnTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var items by remember {
+                mutableStateOf(
+                    (1..20).map {
+                        ListItem (
+                            title = "item$it",
+                            isSelected = false
+                        )
+                    }
+                )
+            }
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                items(items.size) { i ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                items = items.mapIndexed { j, item ->
+                                    if(i == j) {
+                                        item.copy(isSelected = !item.isSelected)
+                                    } else item
+                                }
+                            }
+                            .padding(16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(text = items[i].title)
+                        if(items[i].isSelected) {
+                            Icon(
+                                imageVector = Icons.Default.Check,
+                                contentDescription = "Selected",
+                                tint = Color.Green,
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
